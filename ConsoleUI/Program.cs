3a7f4d7b8e13ff30a6b7a788e18187827c086136
Krishna Prasad Vijayarajan <krishnaprasadvijayarajan@users.noreply.github.com>
2021-01-05 17:46:41 +0000

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleUI;
+using System;
+
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static TableServers host1List = TableServers.GetTableServers();
+        static TableServers host2List = TableServers.GetTableServers();
+
+        static void Main(string[] args)
+        {
+            TableServers servers = TableServers.GetTableServers();
+
+            for (int i = 0; i < 5; i++)
+            {
+                //Console.WriteLine("Next server is : " + servers.GetNextServer());
+                Host1GetNextServer();
+                Host2GetNextServer();
+            }
+            Console.ReadLine();
+        }
+        private static void Host1GetNextServer()
+        {
+            Console.WriteLine("The next server is : " + host1List.GetNextServer());
+        }
+
+        private static void Host2GetNextServer()
+        {
+            Console.WriteLine("The next server is : " + host2List.GetNextServer());
+        }
+    }
+}
---
+++ ConsoleUI/TableServers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class TableServers
+    {
+        private static readonly TableServers _instance = new();
+        private List<string> servers = new();
+        private int nextServer = 0;
+        private TableServers()
+        {
+            servers.Add("Server1");
+            servers.Add("Server2");
+            servers.Add("Server3");
+            servers.Add("Server4");
+        }
+
+        public static TableServers GetTableServers()
+        {
+            return _instance;
+        }
+        public string GetNextServer()
+        {
+            string output = servers[nextServer];
+            nextServer += 1;
+            if(nextServer >=servers.Count)
+            {
+                nextServer = 0;
+            }
+            return output;
+        }
+    }
+}
